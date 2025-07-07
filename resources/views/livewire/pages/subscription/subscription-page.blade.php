78c702e7e8d7f22edce2472025fadfe6741d41d5
junskii <arjuna.andio@binus.ac.id>
2025-07-07 08:25:55 +0700

    Added vendor subscription page.

--- resources/views/livewire/pages/subscription/subscription-page.blade.php
+++ resources/views/livewire/pages/subscription/subscription-page.blade.php
@@ -1,116 +1,113 @@
-<div>
-    <div class="relative min-h-screen">
-        <div class="relative flex flex-col px-6 md:px-16 lg:px-48">
-            <div class="py-10 md:py-12">
-                <button
-                    class="w-8 h-8 md:w-10 md:h-10 rounded-full bg-Ecru hover:bg-white transition-all duration-300 flex items-center justify-center shadow">
-                    <svg width="14" height="28" viewBox="0 0 14 28" fill="none" xmlns="http://www.w3.org/2000/svg"
-                        class="w-4 md:w-6 h-6 text-gray-700">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M2.15016 13.1704L8.74999 6.57059L10.3997 8.22026L4.62466 13.9953L10.3997 19.7703L8.74999 21.4199L2.15016 14.8201C1.93144 14.6013 1.80857 14.3046 1.80857 13.9953C1.80857 13.6859 1.93144 13.3892 2.15016 13.1704Z"
-                            fill="#6B592E" />
-                    </svg>
-                </button>
+<div class="relative min-h-screen">
+    <div class="relative flex flex-col px-6 md:px-16 lg:px-48">
+        <div class="py-10 md:py-12">
+            <button
+                class="w-8 h-8 md:w-10 md:h-10 rounded-full bg-Ecru hover:bg-white transition-all duration-300 flex items-center justify-center shadow">
+                <svg width="14" height="28" viewBox="0 0 14 28" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    class="w-4 md:w-6 h-6 text-gray-700">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M2.15016 13.1704L8.74999 6.57059L10.3997 8.22026L4.62466 13.9953L10.3997 19.7703L8.74999 21.4199L2.15016 14.8201C1.93144 14.6013 1.80857 14.3046 1.80857 13.9953C1.80857 13.6859 1.93144 13.3892 2.15016 13.1704Z"
+                        fill="#6B592E" />
+                </svg>
+            </button>
+        </div>
+
+        {{-- HEADER --}}
+        <div class="flex flex-col justify-center items-center h-full w-full">
+            <div class="text-center font-Kuunari text-4xl mb-4">
+                <h1 class="text-Ecru">BISNIS BARBERSHOP KAMU, LEVEL-UP SEKARANG.</h1>
             </div>
-
-            {{-- HEADER --}}
-            <div class="flex flex-col justify-center items-center h-full w-full">
-                <div class="text-center font-Kuunari text-4xl mb-4">
-                    <h1 class="text-Ecru">BISNIS BARBERSHOP KAMU, LEVEL-UP SEKARANG.</h1>
-                </div>
-                <div class="text-center font-poppins text-xl max-w-[75%] mb-4">
-                    <p>Kamu punya usaha barbershop dan mau tampil lebih profesional? Pakai fitur Seecut: dari sistem
-                        booking otomatis, halaman profil digital, sampai laporan harian—semua langsung dari satu tempat.
-                        Kelola bisnismu tanpa ribet!</p>
-                </div>
-                <div class="container-card w-[75%] flex flex-row justify-between">
+            <div class="text-center font-poppins text-xl max-w-[75%] mb-4">
+                <p>Kamu punya usaha barbershop dan mau tampil lebih profesional? Pakai fitur Seecut: dari sistem
+                    booking otomatis, halaman profil digital, sampai laporan harian—semua langsung dari satu tempat.
+                    Kelola bisnismu tanpa ribet!</p>
+            </div>
+            <div class="container-card w-[75%] flex flex-row justify-between">
+                <div
+                    class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
+        transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
+                    <!-- Garis atas -->
                     <div
-                        class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
-            transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
-                        <!-- Garis atas -->
-                        <div
-                            class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
-                        </div>
-
-                        <!-- Isi Card -->
-                        <div class="p-5 flex flex-col gap-y-4">
-                            <h3>BASIC PLAN</h3>
-                            <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">99K</h1>
-
-                            <div class="flex flex-col gap-y-3 text-white">
-                                <h3>3 Bulan</h3>
-                                <h3>Fitur Booking Otomatis</h3>
-                                <h3>Halaman Profil Digital</h3>
-                            </div>
-
-                            <div class="pt-4">
-                                <button
-                                    class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
-                                    LANGGANAN SEKARANG
-                                </button>
-                            </div>
-                        </div>
+                        class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
                     </div>
 
-                    <div
-                        class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
-            transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
-                        <!-- Garis atas -->
-                        <div
-                            class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
+                    <!-- Isi Card -->
+                    <div class="p-5 flex flex-col gap-y-4">
+                        <h3>BASIC PLAN</h3>
+                        <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">99K</h1>
+
+                        <div class="flex flex-col gap-y-3 text-white">
+                            <h3>3 Bulan</h3>
+                            <h3>Fitur Booking Otomatis</h3>
+                            <h3>Halaman Profil Digital</h3>
                         </div>
 
-                        <!-- Isi Card -->
-                        <div class="p-5 flex flex-col gap-y-4">
-                            <h3>BASIC PLAN</h3>
-                            <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">99K</h1>
-
-                            <div class="flex flex-col gap-y-3 text-white">
-                                <h3>3 Bulan</h3>
-                                <h3>Fitur Booking Otomatis</h3>
-                                <h3>Halaman Profil Digital</h3>
-                            </div>
-
-                            <div class="pt-4">
-                                <button
-                                    class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
-                                    LANGGANAN SEKARANG
-                                </button>
-                            </div>
+                        <div class="pt-4">
+                            <button
+                                class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
+                                LANGGANAN SEKARANG
+                            </button>
                         </div>
                     </div>
-
-                    <div
-                        class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
-            transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
-                        <!-- Garis atas -->
-                        <div
-                            class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
-                        </div>
-
-                        <!-- Isi Card -->
-                        <div class="p-5 flex flex-col gap-y-4">
-                            <h3>BASIC PLAN</h3>
-                            <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">99K</h1>
-
-                            <div class="flex flex-col gap-y-3 text-white">
-                                <h3>3 Bulan</h3>
-                                <h3>Fitur Booking Otomatis</h3>
-                                <h3>Halaman Profil Digital</h3>
-                            </div>
-
-                            <div class="pt-4">
-                                <button
-                                    class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
-                                    LANGGANAN SEKARANG
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
+
+                <div
+                    class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
+        transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
+                    <!-- Garis atas -->
+                    <div
+                        class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
+                    </div>
+
+                    <!-- Isi Card -->
+                    <div class="p-5 flex flex-col gap-y-4">
+                        <h3>STANDARD PLAN</h3>
+                        <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">179K</h1>
+
+                        <div class="flex flex-col gap-y-3 text-white">
+                            <h3>6 Bulan</h3>
+                            <h3>Fitur Booking Otomatis</h3>
+                            <h3>Halaman Profil Digital</h3>
+                        </div>
+
+                        <div class="pt-4">
+                            <button
+                                class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
+                                LANGGANAN SEKARANG
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    class="w-[30%] h-[390px] bg-Eerie-Black flex flex-col gap-y-2 justify-start text-center rounded-md 
+        transition-all duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-lg group">
+                    <!-- Garis atas -->
+                    <div
+                        class="h-1 bg-Ecru w-full rounded-t-md transition-all duration-300 group-hover:bg-Satin-Sheen-Yellow">
+                    </div>
+
+                    <!-- Isi Card -->
+                    <div class="p-5 flex flex-col gap-y-4">
+                        <h3>PRO PLAN</h3>
+                        <h1 class="text-8xl font-Kuunari text-Satin-Sheen-Yellow text-shadow-up">299K</h1>
+
+                        <div class="flex flex-col gap-y-3 text-white">
+                            <h3>12 Bulan</h3>
+                            <h3>Fitur Booking Otomatis</h3>
+                            <h3>Halaman Profil Digital</h3>
+                        </div>
+
+                        <div class="pt-4">
+                            <button
+                                class="w-full py-3 bg-Satin-Sheen-Yellow text-Taupe rounded-sm font-Kuunari text-xl">
+                                LANGGANAN SEKARANG
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-</div>
 </div>
