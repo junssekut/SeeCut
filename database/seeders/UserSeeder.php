3579af78032979235bbeef68b673aabcc9d39b46
junskii <arjuna.andio@binus.ac.id>
2025-07-25 08:40:40 +0700

    User seeder addition for three main roles: user, vendor, and admin.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory()->create([
+            'username' => 'arjunaandio',
+            'email' => 'arzunadio@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user->profile()->updateOrCreate([], [
+            'first_name' => 'Arjuna',
+            'last_name' => 'Andio',
+            'role' => 'customer',
+            'gender' => 'male',
+            'birth_date' => '1995-03-15',
+        ]);
+
+        $vendor = User::factory()->create([
+            'username' => 'jelvisanggono',
+            'email' => 'jelvisanggono@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $vendor->profile()->updateOrCreate([], [
+            'first_name' => 'Jelvis',
+            'last_name' => 'Anggono',
+            'role' => 'vendor',
+            'gender' => 'male',
+            'birth_date' => '1992-08-22',
+        ]);
+
+        $admin = User::factory()->create([
+            'username' => 'matthewraditya',
+            'email' => 'matthewraditya03@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $admin->profile()->updateOrCreate([], [
+            'first_name' => 'Matthew',
+            'last_name' => 'Raditya',
+            'role' => 'admin',
+            'gender' => 'male',
+            'birth_date' => '1990-12-03',
+        ]);
+    }
+}
